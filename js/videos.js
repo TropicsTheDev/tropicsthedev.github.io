bef9197c9772e2ca390480e0fee350d86fa06bd1
Douglas Townsend <dougymt@gmail.com>
2020-03-24 17:45:23 -0400

    video js file potential search integration

--- js/videos.js
+++ js/videos.js
@@ -1,9 +1,16 @@
 const videoList = {
     videos: [
-        {id: 1, title: "BJJ Closed Guard Principals", link: "https://www.youtube.com/watch?v=KKxD5kdOkk0", keys: ["fundamentals", "guard", "closed guard", "beginner"]},
-        {id: 2, title: "Half Guard 101", link: "https://www.youtube.com/watch?v=9zKr-xYlPcg", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps"]},
-        {id: 3, title: "Half Guard 101", link: "https://www.youtube.com/watch?v=9zKr-xYlPcg", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps"]},
-        
-
+        {id: 1, title: "BJJ Closed Guard Principals", link: "https://www.youtube.com/watch?v=KKxD5kdOkk0", keys: ["fundamentals", "guard", "closed guard", "beginner", "defense"]},
+        {id: 2, title: "Half Guard 101", link: "https://www.youtube.com/watch?v=9zKr-xYlPcg", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps" , "defense"]},
+        {id: 3, title: "The Open Guard: Principals and Concepts", link: "https://www.youtube.com/watch?v=mKiXbVLHNNQ", keys: ["fundamentals", "guard", "open guard", "beginner", "sweeps", "defense"]},
+        {id: 4, title: "Maintaining Mount", link: "https://www.youtube.com/watch?v=Wy6Si0FuQaU", keys: ["fundamentals", "mount", "full mount", "controls", "beginner"]},
+        {id: 5, title: "How to Hold Side Control", link: "https://www.youtube.com/watch?v=5dRzDEnFTqY", keys: ["fundamentals", "side control", "controls", "beginner"]},
+        {id: 6, title: "", link: "https://www.youtube.com/watch?v=mKiXbVLHNNQ", keys: ["fundamentals", "guard", "open guard", "beginner", "sweeps"]},
+        {id: 7, title: "5 Chokes Everyone Needs to know", link: "https://www.youtube.com/watch?v=SrWZLEJLOmo", keys: ["fundamentals", "submissions", "chokes", "beginner", "strangles", "attacks", "offense"]},
+        {id: 8, title: "White Belt Basic Armbar", link: "https://www.youtube.com/watch?v=mKiXbVLHNNQ", keys: ["fundamentals", "guard", "closed guard", "beginner", "attacks", "armbars", "armlocks", "submissions"]},
+        {id: 9, title: "Ashi Garami Into a Powerful Footlock", link: "https://www.youtube.com/watch?v=59hN1vNz7VM", keys: ["fundamentals", "guard", "open guard", "beginner", "submissions", "leglocks", "ashi garami", "attacks"]},
+        {id: 10, title: "Flower Sweep", link: "https://www.youtube.com/watch?v=JrW9IIlYs14", keys: ["fundamentals", "guard", "closed guard", "beginner", "sweeps", "defense"]},
+        {id: 11, title: "Deep Half Guard Sweep", link: "https://www.youtube.com/watch?v=WNXrZjz8nFU", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps", "deep half guard"]},
+        {id: 12, title: "Guillotine Choke", link: "https://www.youtube.com/watch?v=mhSri-cxfC8", keys: ["fundamentals", "submissions", "open guard", "beginner", "sweeps", "chokes", "strangles", "controls", "mount"]}
     ]
 }
