6febc51886cf78f8361bd416bbb3014dd084e3a5
Douglas Townsend <dougymt@gmail.com>
2020-03-25 13:52:59 -0400

    figured out video espect ratio.

--- js/videos.js
+++ js/videos.js
@@ -1,16 +1,140 @@
 const videoList = {
-    videos: [
-        {id: 1, title: "BJJ Closed Guard Principals", link: "https://www.youtube.com/embed/KKxD5kdOkk0", keys: ["fundamentals", "guard", "closed guard", "beginner", "defense"]},
-        {id: 2, title: "Half Guard 101", link: "https://www.youtube.com/embed/9zKr-xYlPcg", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps" , "defense"]},
-        {id: 3, title: "The Open Guard: Principals and Concepts", link: "https://www.youtube.com/embed/mKiXbVLHNNQ", keys: ["fundamentals", "guard", "open guard", "beginner", "sweeps", "defense"]},
-        {id: 4, title: "Maintaining Mount", link: "https://www.youtube.com/embed/Wy6Si0FuQaU", keys: ["fundamentals", "mount", "full mount", "controls", "beginner"]},
-        {id: 5, title: "How to Hold Side Control", link: "https://www.youtube.com/embed/5dRzDEnFTqY", keys: ["fundamentals", "side control", "controls", "beginner"]},
-        {id: 6, title: "Back Mount Basics", link: "https://www.youtube.com/embed/1fGNEy3HIrA", keys: ["fundamentals", "guard", "open guard", "beginner", "sweeps"]},
-        {id: 7, title: "5 Chokes Everyone Needs to know", link: "https://www.youtube.com/embed/SrWZLEJLOmo", keys: ["fundamentals", "submissions", "chokes", "beginner", "strangles", "attacks", "offense"]},
-        {id: 8, title: "White Belt Basic Armbar", link: "https://www.youtube.com/embed/mMQjtRBE1uA", keys: ["fundamentals", "guard", "closed guard", "beginner", "attacks", "armbars", "armlocks", "submissions"]},
-        {id: 9, title: "Ashi Garami Into a Powerful Footlock", link: "https://www.youtube.com/embed/59hN1vNz7VM", keys: ["fundamentals", "guard", "open guard", "beginner", "submissions", "leglocks", "ashi garami", "attacks"]},
-        {id: 10, title: "Flower Sweep", link: "https://www.youtube.com/embed/JrW9IIlYs14", keys: ["fundamentals", "guard", "closed guard", "beginner", "sweeps", "defense"]},
-        {id: 11, title: "Deep Half Guard Sweep", link: "https://www.youtube.com/embed/WNXrZjz8nFU", keys: ["fundamentals", "guard", "half guard", "beginner", "sweeps", "deep half guard"]},
-        {id: 12, title: "Guillotine Choke", link: "https://www.youtube.com/embed/mhSri-cxfC8", keys: ["fundamentals", "submissions", "open guard", "beginner", "sweeps", "chokes", "strangles", "controls", "mount"]}
-    ]
-}
+  videos: [
+    {
+      id: 1,
+      title: "BJJ Closed Guard Principals",
+      link: "https://www.youtube.com/embed/KKxD5kdOkk0",
+      keys: ["fundamentals", "guard", "closed guard", "beginner", "defense"]
+    },
+    {
+      id: 2,
+      title: "Half Guard 101",
+      link: "https://www.youtube.com/embed/9zKr-xYlPcg",
+      keys: [
+        "fundamentals",
+        "guard",
+        "half guard",
+        "beginner",
+        "sweeps",
+        "defense"
+      ]
+    },
+    {
+      id: 3,
+      title: "The Open Guard: Principals and Concepts",
+      link: "https://www.youtube.com/embed/mKiXbVLHNNQ",
+      keys: [
+        "fundamentals",
+        "guard",
+        "open guard",
+        "beginner",
+        "sweeps",
+        "defense"
+      ]
+    },
+    {
+      id: 4,
+      title: "Maintaining Mount",
+      link: "https://www.youtube.com/embed/Wy6Si0FuQaU",
+      keys: ["fundamentals", "mount", "full mount", "controls", "beginner"]
+    },
+    {
+      id: 5,
+      title: "How to Hold Side Control",
+      link: "https://www.youtube.com/embed/5dRzDEnFTqY",
+      keys: ["fundamentals", "side control", "controls", "beginner"]
+    },
+    {
+      id: 6,
+      title: "Back Mount Basics",
+      link: "https://www.youtube.com/embed/1fGNEy3HIrA",
+      keys: ["fundamentals", "guard", "open guard", "beginner", "sweeps"]
+    },
+    {
+      id: 7,
+      title: "5 Chokes Everyone Needs to know",
+      link: "https://www.youtube.com/embed/SrWZLEJLOmo",
+      keys: [
+        "fundamentals",
+        "submissions",
+        "chokes",
+        "beginner",
+        "strangles",
+        "attacks",
+        "offense"
+      ]
+    },
+    {
+      id: 8,
+      title: "White Belt Basic Armbar",
+      link: "https://www.youtube.com/embed/mMQjtRBE1uA",
+      keys: [
+        "fundamentals",
+        "guard",
+        "closed guard",
+        "beginner",
+        "attacks",
+        "armbars",
+        "armlocks",
+        "submissions"
+      ]
+    },
+    {
+      id: 9,
+      title: "Ashi Garami Into a Powerful Footlock",
+      link: "https://www.youtube.com/embed/59hN1vNz7VM",
+      keys: [
+        "fundamentals",
+        "guard",
+        "open guard",
+        "beginner",
+        "submissions",
+        "leglocks",
+        "ashi garami",
+        "attacks"
+      ]
+    },
+    {
+      id: 10,
+      title: "Flower Sweep",
+      link: "https://www.youtube.com/embed/JrW9IIlYs14",
+      keys: [
+        "fundamentals",
+        "guard",
+        "closed guard",
+        "beginner",
+        "sweeps",
+        "defense"
+      ]
+    },
+    {
+      id: 11,
+      title: "Deep Half Guard Sweep",
+      link: "https://www.youtube.com/embed/WNXrZjz8nFU",
+      keys: [
+        "fundamentals",
+        "guard",
+        "half guard",
+        "beginner",
+        "sweeps",
+        "deep half guard"
+      ]
+    },
+    {
+      id: 12,
+      title: "Guillotine Choke",
+      link: "https://www.youtube.com/embed/mhSri-cxfC8",
+      keys: [
+        "fundamentals",
+        "submissions",
+        "open guard",
+        "beginner",
+        "sweeps",
+        "chokes",
+        "strangles",
+        "controls",
+        "mount"
+      ]
+    }
+  ]
+};
